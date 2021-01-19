0819fbf2c26a21787a27c415ec12dc53c65e30d4
sam <sblan45@gmail.com>
2021-01-18 21:04:07 -0500

    adds App and SearchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  InputChange = () => {
+    
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input 
+              type="text" 
+              value={this.state.searchTerm}
+              onChange={this.InputChange}
+             />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar

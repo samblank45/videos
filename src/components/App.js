a3d7dc179a313ae9e5ab957614e3e06cdce14a25
sam <sblan45@gmail.com>
2021-01-25 21:14:44 -0500

    adds VideoDetail and deeply nested callback

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
   }
   searchTermSubmit = async (term) => {
@@ -20,11 +22,19 @@ class App extends React.Component {
     this.setState({videos: response.data.items})
   }
 
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video})
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.searchTermSubmit}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={this.onVideoSelect} 
+          videos={this.state.videos} 
+        />
       </div>
     )
   }

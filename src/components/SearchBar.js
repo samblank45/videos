692eed51338a32849e040172d6e1db47c2121b20
sam <sblan45@gmail.com>
2021-01-26 21:45:10 -0500

    adds search button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -22,16 +22,17 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input 
-              type="text" 
-              value={this.state.searchTerm}
-              onChange={this.handleChange}
-             />
-          </div>
-        </form>
+          <form onSubmit={this.onFormSubmit} className="ui form">
+            <div className="field">
+                <label>Video Search</label>
+                <input 
+                  type="text" 
+                  value={this.state.searchTerm}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button>search</button>
+          </form>      
       </div>
     )
   }
